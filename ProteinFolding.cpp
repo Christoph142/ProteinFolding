d5f84bed80a50454a56018b703e6893133e9223f
Christoph142 <Christoph142@gmx.com>
2014-07-04 08:39:16 +0200

    Abnahme

--- ProteinFolding.cpp
+++ ProteinFolding.cpp
@@ -15,8 +15,8 @@ int main() {
     const string SEQ10 = "1001011100";
     const string SEQ20 = "10100110100101100101"; // max 9
     const string SEQ24 = "110010010010010010010011";
-	const string SEQ25  = "1001010010100101001101111"; // max 12
-	const string ABNAHME = "";
+	const string TEST  = "1001010010100101001101111"; // max 12
+	const string ABNAHME = "1101000000000101000110101";
     const string SEQ25 = "0010011000011000011000011";
     const string SEQ36 = "000110011000001111111001100001100100";
     const string SEQ48 = "001001100110000011111111110000001100110010011111";
@@ -27,7 +27,7 @@ int main() {
     //for (int testingSeed = 0; testingSeed < 1; testingSeed++) {
         srand(time(NULL));
         //cout << "Seed " << testingSeed << "..." << endl;*/
-        Population p(SEQ25, 1000);
+        Population p(ABNAHME, 1000);
 
         for (int generation = 1; generation <= maxGenerations; generation++) {
             cout << generation << ". Generation" << endl;
